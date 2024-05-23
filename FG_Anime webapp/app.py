0dd470f2e482d8f99a4e7acd36d9c11e65b191dc
Furjack <81816200+furjac@users.noreply.github.com>
2024-05-23 07:08:41 +0530

    the web app is ready now only thing is implement number of episodes in an anime

--- FG_Anime webapp/app.py
+++ FG_Anime webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
@@ -7,9 +7,12 @@ def format_anime_name(name):
 
 @app.route('/')
 def index():
-    episode = request.args.get('episode', '90')
-    embed_url = f"https://2anime.xyz/embed/one-piece-episode-{episode}"
-    return render_template('index.html', embed_url=embed_url, episode=episode, episode_start=int(episode))
+    episode = int(request.args.get('episode', '1'))
+    episode_type = request.args.get('type', 'sub')
+    embed_url = f"https://2anime.xyz/embed/one-piece{'-dub' if episode_type == 'dub' else ''}-episode-{episode}"
+    response = make_response(render_template('index.html', embed_url=embed_url, episode=episode, episode_start=(episode - 1) // 100 * 100 + 1, episode_type=episode_type))
+    response.set_cookie('last_episode', str(episode), max_age=30*24*60*60)  # Save for 30 days
+    return response
 
 @app.route('/search')
 def search():
@@ -21,10 +24,13 @@ def search():
 
 @app.route('/anime/<name>')
 def anime(name):
-    episode = request.args.get('episode', '1')
-    episode_start = int(episode) // 100 * 100 + 1
-    embed_url = f"https://2anime.xyz/embed/{name}-episode-{episode}"
-    return render_template('anime.html', embed_url=embed_url, anime_name=name, episode=episode, episode_start=episode_start)
+    episode = int(request.args.get('episode', request.cookies.get('last_episode', '1')))
+    episode_type = request.args.get('type', 'sub')
+    episode_start = (episode - 1) // 100 * 100 + 1
+    embed_url = f"https://2anime.xyz/embed/{name}{'-dub' if episode_type == 'dub' else ''}-episode-{episode}"
+    response = make_response(render_template('anime.html', embed_url=embed_url, anime_name=name, episode=episode, episode_start=episode_start, episode_type=episode_type))
+    response.set_cookie(f'{name}_last_episode', str(episode), max_age=30*24*60*60)  # Save for 30 days
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
