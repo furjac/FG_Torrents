9cc668d98d5d9cf677706ea736b61eec279057d3
Furjack <81816200+furjac@users.noreply.github.com>
2024-05-27 06:45:25 +0530

    Webapp minor issues fixed

--- FG_Anime webapp/app.py
+++ FG_Anime webapp/app.py
@@ -3,17 +3,22 @@ import requests
 
 app = Flask(__name__)
 
+
 def format_anime_name(name):
     return name.lower().replace(' ', '-')
+
 
 @app.route('/')
 def index():
     episode = int(request.args.get('episode', '1'))
     episode_type = request.args.get('type', 'sub')
     embed_url = f"https://2anime.xyz/embed/one-piece{'-dub' if episode_type == 'dub' else ''}-episode-{episode}"
-    response = make_response(render_template('index.html', embed_url=embed_url, episode=episode, episode_start=(episode - 1) // 100 * 100 + 1, episode_type=episode_type))
-    response.set_cookie('last_episode', str(episode), max_age=30*24*60*60)  # Save for 30 days
+    response = make_response(render_template('index.html', embed_url=embed_url, episode=episode, episode_start=(
+        episode - 1) // 100 * 100 + 1, episode_type=episode_type))
+    response.set_cookie('last_episode', str(episode),
+                        max_age=30*24*60*60)  # Save for 30 days
     return response
+
 
 @app.route('/search')
 def search():
@@ -22,6 +27,7 @@ def search():
         formatted_name = format_anime_name(query)
         return redirect(url_for('details', name=formatted_name))
     return redirect(url_for('index'))
+
 
 @app.route('/details/<name>')
 def details(name):
@@ -56,20 +62,27 @@ def details(name):
     else:
         return "Anime not found."
 
+
 @app.route('/anime/<name>')
 def anime(name):
-    episode = int(request.args.get('episode', request.cookies.get(f'{name}_last_episode', '1')))
+    episode = int(request.args.get(
+        'episode', request.cookies.get(f'{name}_last_episode', '1')))
     episode_type = request.args.get('type', 'sub')
     episode_start = (episode - 1) // 100 * 100 + 1
     embed_url = f"https://2anime.xyz/embed/{name}{'-dub' if episode_type == 'dub' else ''}-episode-{episode}"
-    response = make_response(render_template('anime.html', embed_url=embed_url, anime_name=name, episode=episode, episode_start=episode_start, episode_type=episode_type))
-    response.set_cookie(f'{name}_last_episode', str(episode), max_age=30*24*60*60)  # Save for 30 days
+    response = make_response(render_template('anime.html', embed_url=embed_url, anime_name=name,
+                             episode=episode, episode_start=episode_start, episode_type=episode_type))
+    response.set_cookie(f'{name}_last_episode', str(
+        episode), max_age=30*24*60*60)  # Save for 30 days
     return response
 
 # Route to serve the favicon
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(app.static_folder, 'favicon.ico')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
